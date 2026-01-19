2974d0429652700e946f00c3985672d1b33254ef
Dakshesh Nankani <daksheshnankani1405@gmail.com>
2026-01-19 17:47:28 +0530

    relaxed orientation constraint

--- arm_ik/cartesian_controller.py
+++ arm_ik/cartesian_controller.py
@@ -4,11 +4,9 @@ from rclpy.action import ActionClient
 
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import JointState
-
 from moveit_msgs.srv import GetCartesianPath, GetPositionFK
-from moveit_msgs.msg import RobotState
+from moveit_msgs.msg import RobotState, RobotTrajectory
 from moveit_msgs.action import ExecuteTrajectory
-
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 import threading, sys, termios, tty
@@ -27,8 +25,14 @@ class CartesianController(Node):
         super().__init__('cartesian_controller')
 
         self.estop_active = False
-        self.current_exec_goal = None
         self.current_joint_state = None
+
+        self.exec_done_event = threading.Event()
+        self.exec_result = None
+        self.current_exec_goal = None
+
+        self.fk_event = threading.Event()
+        self.last_fk_pose = None
 
         self.create_subscription(JointState, '/joint_states', self.joint_state_cb, 10)
 
@@ -53,16 +57,8 @@ class CartesianController(Node):
 
         threading.Thread(target=self.keyboard_listener, daemon=True).start()
 
-        self.get_logger().info('Controls: [SPACE]=E-SYOP | [h]=HOME | [r]=RESET')
+        self.get_logger().info('Controls: [SPACE]=E-STOP | [h]=HOME | [r]=RESET | [t]=TEST')
 
-        # self.move_linear(0.0, 0.0, 0.05)
-
-    def joint_state_cb(self, msg: JointState):
-        self.current_joint_state = msg
-
-    def get_current_joint_state(self):
-        return self.current_joint_state
-    
     def keyboard_listener(self):
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
@@ -78,45 +74,50 @@ class CartesianController(Node):
                     self.go_home()
                 elif key == 'r':
                     self.reset_estop()
+                elif key == 't':
+                    self.test_cartesian_sequence()
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
-    def trigger_estop(self):
-        if self.estop_active:
-            return
-        
+    def joint_state_cb(self, msg: JointState):
+        self.current_joint_state = msg
+
+    def trigger_estop(self):        
         self.estop_active = True
         self.get_logger().error("!!! EMERGENCY STOP ACTIVATED !!!")
 
         if self.current_exec_goal:
             self.current_exec_goal.cancel_goal_async()
 
-    def reset_estop(self):
-        self.estop_active = False
-        self.get_logger().info('E-STOP cleared')
+    def execute_trajectory(self, traj: JointTrajectory):
+        self.exec_done_event.clear()
 
-    def get_current_ee_pose(self):
-        if self.current_joint_state is None:
-            self.get_logger().error('No joint state yet')
-            return None
+        goal = ExecuteTrajectory.Goal()
 
-        req = GetPositionFK.Request()
-        req.header.frame_id = 'base_link'
-        req.fk_link_names = ['end_effector_link']
-        req.robot_state = RobotState()
-        req.robot_state.joint_state = self.current_joint_state
-
-        future = self.fk_client.call_async(req)
-        rclpy.spin_until_future_complete(self, future)
-
-        res = future.result()
-
-        if not res or not res.pose_stamped:
-            self.get_logger().error("FK Failed")
-            return None
+        robot_traj = RobotTrajectory()
+        robot_traj.joint_trajectory = traj
+        goal.trajectory = robot_traj
         
-        return res.pose_stamped[0].pose
+        future = self.exec_client.send_goal_async(goal)
+        future.add_done_callback(self._on_exec_goal)
+
+        self.exec_done_event.wait()
+
+        return self.exec_result.error_code.val == 1
     
+    def _on_exec_goal(self, future):
+        gh = future.result()
+
+        if not gh.accepted:
+            self.exec_done_event.set()
+            return
+        
+        self.current_exec_goal = gh
+        gh.get_result_async().add_done_callback(self._on_exec_result)
+
+    def _on_exec_result(self, future):
+        self.exec_result = future.result().result
+        self.exec_done_event.set()
 
     def go_home(self):
         if self.estop_active:
@@ -140,18 +141,69 @@ class CartesianController(Node):
         home = JointTrajectoryPoint()
         home.positions = list(HOME_JOINTS[j] for j in traj.joint_names)
         home.time_from_start.sec = 3
-        traj.points.append(home)
-
-        goal = ExecuteTrajectory.Goal()
-        goal.trajectory.joint_trajectory = traj
+        
+        traj.points = [start, home]
 
         self.get_logger().info("Moving to HOME pose...")
+        self.execute_trajectory(traj)
 
-        future = self.exec_client.send_goal_async(goal)
-        future.add_done_callback(self.store_exec_goal)
+    def get_current_ee_pose(self):
+        if self.current_joint_state is None:
+            self.get_logger().error('No joint state yet')
+            return None
+        
+        self.fk_event.clear()
 
-    def store_exec_goal(self, future):
-        self.current_exec_goal = future.result()
+        req = GetPositionFK.Request()
+        req.header.frame_id = 'base_link'
+        req.fk_link_names = ['end_effector_link']
+        req.robot_state = RobotState()
+        req.robot_state.joint_state = self.current_joint_state
+
+        future = self.fk_client.call_async(req)
+        future.add_done_callback(self._on_fk_response)
+
+        if not self.fk_event.wait(1.0):
+            self.get_logger().error("FK timeout")
+            return None
+        
+        return self.last_fk_pose
+
+    def wait_for_execution(self, timeout=10.0):
+        finished = self.exec_done_event.wait(timeout)
+
+        if not finished:
+            self.get_logger().error("Execution timed out")
+            return False
+        
+        self.exec_done_event.clear()
+
+        if self.exec_result.error_code.val != 1:
+            self.get_logger().error(f"Execution failed with code {self.exec_result.error_code.val}")
+            return False
+        
+        return True
+    
+    def test_cartesian_sequence(self):
+        if self.estop_active:
+            self.get_logger().error('E-STOP active, aborting sequence')
+            return
+        
+        self.move_linear(0.1, 0.0, 0.0)
+        self.wait_for_execution()
+
+        self.move_linear(0.0, 0.0, -0.1)
+        self.wait_for_execution()
+
+        self.move_linear(-0.1, 0.0, 0.0)
+        self.wait_for_execution()
+
+    def get_current_joint_state(self):
+        return self.current_joint_state
+
+    def reset_estop(self):
+        self.estop_active = False
+        self.get_logger().info('E-STOP cleared')
 
     def within_workspace(self, pose: Pose) -> bool:
         x, y, z = pose.position.x, pose.position.y, pose.position.z
@@ -172,7 +224,6 @@ class CartesianController(Node):
 
             if point.accelerations:
                 point.accelerations = [a * self.accel_scale for a in point.accelerations]
-
     
     def move_linear(self, dx, dy, dz):
         if self.estop_active:
@@ -186,15 +237,9 @@ class CartesianController(Node):
         target_pose.position.x = start_pose.position.x + dx
         target_pose.position.y = start_pose.position.y + dy
         target_pose.position.z = start_pose.position.z + dz
-        target_pose.orientation = start_pose.orientation
 
         if not self.within_workspace(target_pose):
-            self.get_logger().error(
-                f'''Target pose out of workspace
-                x={target_pose.position.x:.2f}
-                y={target_pose.position.y:.2f}
-                z={target_pose.position.z:.2f}'''
-            )
+            self.get_logger().error("Target pose out of workspace")
 
         req = GetCartesianPath.Request()
         req.group_name = 'arm'
@@ -221,42 +266,44 @@ class CartesianController(Node):
 
         self.scale_trajectory(res.solution.joint_trajectory)
 
-        goal = ExecuteTrajectory.Goal()
-        goal.trajectory = res.solution
+        self.execute_trajectory(res.solution.joint_trajectory)
+
+    def _on_fk_response(self, future):
+        try:
+            res = future.result()
+        except Exception as e:
+            self.get_logger().error(f"FK service failed: {e}")
+            self.fk_event.set()
+            return
+
+        if not res or not res.pose_stamped:
+            self.get_logger().error('FK failed')
+            return
+    
+        self.last_fk_pose = res.pose_stamped[0].pose
+        self.fk_event.set()
+
+    def cartesian_response_callback(self, future):
+        if self.estop_active:
+            return
+        
+        res = future.result()
+
+        self.get_logger().info(f'Cartesian fraction: {res.fraction:.2f}')
+
+        if res.fraction < 0.95:
+            self.get_logger().error('Cartesian path incomplete - aborting')
+            rclpy.shutdown()
+            return
+        
+        self.scale_trajectory(res.solution.joint_trajectory)
+
+        exec_goal = ExecuteTrajectory.Goal()
+        exec_goal.trajectory = res.solution
 
         self.get_logger().info('Executing Cartesian Trajectory...')
-        self.exec_client.send_goal_async(goal)
-
-    # def fk_response_callback(self, future):
-    #     res = future.result()
-
-    #     if not res or not res.pose_stamped:
-    #         self.get_logger().error('FK failed')
-    #         return
-        
-    #     self.curremt_ee_pose = res.pose_stamped[0].pose
-
-
-    # def cartesian_response_callback(self, future):
-    #     if self.estop_active:
-    #         return
-        
-    #     res = future.result()
-
-    #     self.get_logger().info(f'Cartesian fraction: {res.fraction:.2f}')
-
-    #     if res.fraction < 0.95:
-    #         self.get_logger().error('Cartesian path incomplete - aborting')
-    #         rclpy.shutdown()
-    #         return
-        
-    #     self.scale_trajectory(res.solution.joint_trajectory)
-
-    #     exec_goal = ExecuteTrajectory.Goal()
-    #     exec_goal.trajectory = res.solution
-
-    #     self.get_logger().info('Executing Cartesian Trajectory...')
-    #     self.exec_client.send_goal_async(exec_goal)
+        future = self.exec_client.send_goal_async(exec_goal)
+        future.add_done_callback(self._on_exec_result)
 
 
 def main():
